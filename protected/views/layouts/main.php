d242418b3442da40ca693072d694c56bf008537a
luis enrique <enrique_pool@live.com.mx>
2012-01-31 11:13:15 -0600

    usuarios

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -29,12 +29,13 @@
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Usuarios', 'url'=>array('/Usuarios/index')),
+				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				//array('label'=>'Contact', 'url'=>array('/site/contact')),
+				//array('label'=>'Usuarios', 'url'=>array('/Usuarios/index')),
 				//array('label'=>'Usuarios', 'url'=>array('/Usuarios/index'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest),
+				array('label'=>'Usuarios ('.Yii::app()->user->name.')', 'url'=>array('/Usuarios/index'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
 	</div><!-- mainmenu -->
