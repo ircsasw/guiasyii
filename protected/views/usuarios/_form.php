4debf69a161571fbf54f037bd9245cb909701f35
luis enrique <enrique_pool@live.com.mx>
2012-01-30 18:13:01 -0600

    proyectoguias

---
+++ protected/views/usuarios/_form.php
@@ -0,0 +1,36 @@
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'usuarios-form',
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'usuario'); ?>
+		<?php echo $form->textField($model,'usuario',array('size'=>40,'maxlength'=>40)); ?>
+		<?php echo $form->error($model,'usuario'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'pass'); ?>
+		<?php echo $form->passwordField($model,'pass',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'pass'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'nombre'); ?>
+		<?php echo $form->textField($model,'nombre',array('size'=>30,'maxlength'=>30)); ?>
+		<?php echo $form->error($model,'nombre'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/usuarios/_view.php
@@ -0,0 +1,20 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('usuario')); ?>:</b>
+	<?php echo CHtml::encode($data->usuario); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('pass')); ?>:</b>
+	<?php echo CHtml::encode($data->pass); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('nombre')); ?>:</b>
+	<?php echo CHtml::encode($data->nombre); ?>
+	<br />
+
+
+</div>
