38b293664d4a622acf34df07a029adc7f5a9b4ca
programador ircssa <Programador@sistemas.(none)>
2012-02-14 10:29:56 -0600

    primer repo

--- protected/views/guias/_catalog.php
+++ protected/views/guias/_catalog.php
@@ -1,8 +1,21 @@
-<h1>Hola mundo!!</h1>
-<?php 
-foreach ($model as $value) {
-	echo ' / '. $value->id;
-	echo ' - '. $value->serie;
-}
 
+<BODY>
+ 
+<h1>Hola mundo!!</h1>
+<table BORDER="1">
+<tr>
+<th scope="row",>ID</th>
+<th scope="row",>SERIE</th>
+</tr>	
+
+<?php 
+foreach ($model as $value)
+{
+	echo "<tr>
+		<td>". $value->id."</td>"; 
+		echo "<td>". $value->serie."</td>
+	</tr>"; 
+	//echo ''. $value->id;
+}
 ?>
+</table>
