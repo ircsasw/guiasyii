ec65e0bda06300a3ce19f7f010ff5c98145cd28c
IRCSASW <ircsasw@gmail.com>
2012-03-28 11:08:23 -0600

    Sobre relaciones

--- protected/models/Usuarios.php
+++ protected/models/Usuarios.php
@@ -64,6 +64,8 @@ class Usuarios extends CActiveRecord
 		// NOTE: you may need to adjust the relation name and the related
 		// class name for the relations automatically generated below.
 		return array(
+			'asignadas' => array(self::HAS_MANY, 'Guias', 'id_asigna'),
+			'bajadas' => array(self::HAS_MANY, 'Guias', 'id_baja'),
 		);
 	}
 
